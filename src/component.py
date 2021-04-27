f38c9ad8149c2a7ca9835b990d5bb9a882353a4b
David Esner <david@keboola.com>
2021-04-27 17:02:27 +0200

    lib version, component base

--- src/component.py
+++ src/component.py
@@ -6,24 +6,29 @@ import csv
 import logging
 from datetime import datetime
 
-from keboola.component.base import ComponentBase
+from keboola.component.base import ComponentBase, UserException
 
 # configuration variables
 KEY_API_TOKEN = '#api_token'
 KEY_PRINT_HELLO = 'print_hello'
-
-# #### Keep for debug
-KEY_DEBUG = 'debug'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
 REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
 REQUIRED_IMAGE_PARS = []
 
-APP_VERSION = '0.0.1'
-
 
 class Component(ComponentBase):
+    """
+        Extends base class for general Python components. Initializes the CommonInterface
+        and performs configuration validation.
+
+        For easier debugging the data folder is picked up by default from `../data` path,
+        relative to working directory.
+
+        If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
+    """
+
     def __init__(self):
         super().__init__(required_parameters=REQUIRED_PARAMETERS,
                          required_image_parameters=REQUIRED_IMAGE_PARS)
@@ -72,6 +77,9 @@ if __name__ == "__main__":
     try:
         comp = Component()
         comp.run()
+    except UserException as exc:
+        logging.exception(exc)
+        exit(1)
     except Exception as exc:
         logging.exception(exc)
         exit(2)
